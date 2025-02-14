caf3641e28016ce86abc02d3f92b4454e5daa4f3
razasoneji <razaissoneji@gmail.com>
2025-02-14 11:34:02 +0530

    Added login sighnup

---
+++ SpringBoot/demo/src/main/java/com/auditing/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.auditing.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
